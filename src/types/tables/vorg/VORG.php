f0951bd542444e8ab35571a4add048fe56c93cbd
Bernhard <mail@schoenberger.be>
2023-10-06 22:03:20 +0200

    add vorg parser

---
+++ src/types/tables/vorg/VORG.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace schoenbergerb\opentype\types\tables;
+
+class VORG extends AbstractTable
+{
+    public int $majorVersion;
+    public int $minorVersion;
+    public int $defaultVertOriginY;
+    public int $numVertOriginYMetrics;
+    public array $vertOriginYMetrics = [];
+
+    public function __construct(string $fontBinaryData)
+    {
+        $index = 0;
+
+        // Parse the VORG header
+        $this->majorVersion = $this->getUInt16($fontBinaryData, $index);
+        $this->minorVersion = $this->getUInt16($fontBinaryData, $index);
+        $this->defaultVertOriginY = $this->getInt16($fontBinaryData, $index);
+        $this->numVertOriginYMetrics = $this->getUInt16($fontBinaryData, $index);
+
+        // Parse the VertOriginYMetrics records
+        for ($i = 0; $i < $this->numVertOriginYMetrics; $i++) {
+            $glyphIndex = $this->getUInt16($fontBinaryData, $index);
+            $vertOriginY = $this->getInt16($fontBinaryData, $index);
+            $this->vertOriginYMetrics[$glyphIndex] = $vertOriginY;
+        }
+    }
+}
