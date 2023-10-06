08985b1fa53419edeec6fd758756969b174992be
Bernhard <mail@schoenberger.be>
2023-10-06 22:13:12 +0200

    add table types

--- src/types/FontDataTables.php
+++ src/types/FontDataTables.php
@@ -4,10 +4,14 @@ namespace schoenbergerb\opentype\types;
 
 use schoenbergerb\opentype\types\tables\cblc\CBLC;
 use schoenbergerb\opentype\types\tables\cmap\Cmap;
+use schoenbergerb\opentype\types\tables\cvar\CVAR;
+use schoenbergerb\opentype\types\tables\cvt\CVT;
 use schoenbergerb\opentype\types\tables\gdef\Gdef;
 use schoenbergerb\opentype\types\tables\head\Head;
 use schoenbergerb\opentype\types\tables\hhea\Hhea;
 use schoenbergerb\opentype\types\tables\loca\Loca;
+use schoenbergerb\opentype\types\tables\prep\PREP;
+use schoenbergerb\opentype\types\tables\VORG;
 
 class FontDataTables
 {
@@ -17,4 +21,8 @@ class FontDataTables
     public Gdef $GDEF;
     public Loca $LOCA;
     public Cblc $CBLC;
+    public VORG $VORG;
+    public CVAR $CVAR;
+    public PREP $PREP;
+    public CVT $CVT;
 }
